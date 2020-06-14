ebaa9298109e7f8dc5f6625329dc472379943a14
“RIYA <riyabansal98@gmail.com>
2020-06-15 00:22:16 +0530

    Added CallBack Handlers

--- src/App.js
+++ src/App.js
@@ -22,43 +22,44 @@ const App = () => {
     }
   ];
 
+  const [searchTerm, setSearchTerm] = React.useState('');
   const handleSearch = event => {
-    console.log(event.target.value);
+    setSearchTerm(event.target.value);
   }
+
+  const searchedStories = stories.filter(story => 
+     story.title.toLowerCase().includes(searchTerm.toLowerCase())
+  );
 
   return (
     <div>
       <h1>My Hacker Stories</h1>
       <Search onSearch={handleSearch}/>
       <hr />
-      <List list={stories} />
+      <List list={searchedStories} />
     </div>
   );
 };
 
 const Search = props => {
-
-  const [searchTerm, setSearchTerm] = React.useState('');
-  const handleChange = event => {
-    setSearchTerm(event.target.value);
-    props.onSearch(event);
-  }
+  
   return (
     <div>
       <label htmlFor="search">Search: </label>
-      <input id="search" type="text" onChange={handleChange}></input>
-      <hr />
-      <p>
-        Searching for <strong>{searchTerm}</strong>.
-      </p>
+      <input id="search" type="text" onChange={props.onSearch}></input>
     </div>
   );
 }
 const List = props =>  
   props.list.map(item => (
-        <div key={item.objectID}>
-          <a href={item.url}>{item.title}</a>
-        </div>
+    <div key={item.objectID}>
+      <span>
+        <a href={item.url}>{item.title}</a>
+      </span>
+      <span>{item.author}</span>
+      <span>{item.num_comments}</span>
+      <span>{item.points}</span>
+    </div>
   )
 );
 
