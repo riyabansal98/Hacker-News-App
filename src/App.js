8330e47fdb43fa8bf25ea3cd062f7b920629efba
“RIYA <riyabansal98@gmail.com>
2020-06-15 13:12:37 +0530

    Added custom hooks

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
 import React from 'react';
 import './App.css';
 
+const useSemiPersistentState = (key, initialState) => {
+  const [value, setValue] = React.useState(
+    localStorage.getItem(key) || initialState
+  );
+
+  React.useEffect (() => {
+    localStorage.setItem(key, value);
+  }, [value, key]);
+
+  return [value, setValue];
+};
+
 const App = () => {
 
   const stories = [
@@ -22,7 +34,8 @@ const App = () => {
     }
   ];
 
-  const [searchTerm, setSearchTerm] = React.useState('');
+  const [searchTerm, setSearchTerm] = useSemiPersistentState('search', 'React');
+  
   const handleSearch = event => {
     setSearchTerm(event.target.value);
   }
@@ -34,33 +47,41 @@ const App = () => {
   return (
     <div>
       <h1>My Hacker Stories</h1>
-      <Search onSearch={handleSearch}/>
+      <Search search={searchTerm} onSearch={handleSearch}/>
       <hr />
       <List list={searchedStories} />
     </div>
   );
 };
 
-const Search = props => {
+const Search = ({search, onSearch}) => {
   
   return (
     <div>
       <label htmlFor="search">Search: </label>
-      <input id="search" type="text" onChange={props.onSearch}></input>
+      <input 
+        id="search" 
+        type="text" 
+        onChange={onSearch}
+        value={search}
+        >
+      </input>
     </div>
   );
 }
-const List = props =>  
-  props.list.map(item => (
-    <div key={item.objectID}>
-      <span>
-        <a href={item.url}>{item.title}</a>
-      </span>
-      <span>{item.author}</span>
-      <span>{item.num_comments}</span>
-      <span>{item.points}</span>
-    </div>
-  )
+
+const List = ({list}) =>  list.map(item => <Item key={item.objectID} item={item}/>);
+
+const Item = ({ item }) => (
+
+  <div key={item.objectID}>
+    <span>
+      <a href={item.url}>{item.title}</a>
+    </span>
+    <span>{item.author}</span>
+    <span>{item.num_comments}</span>
+    <span>{item.points}</span>
+  </div>
 );
 
 
